d087fd5538d1d4a55bac2ed62b7c0d253e60f032
Steven Torres <stevetorres7562@gmail.com>
2020-06-09 19:20:53 -0400

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Steven Torres on 6/7/20.
+//  Copyright © 2020 Steven Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
